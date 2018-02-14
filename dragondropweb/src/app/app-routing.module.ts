a1e84c0cde31b79a2914a210085fdf9b93b5c0a2
Luna Neumeister <luna.neumeister@proton.me>
2018-02-13 16:31:30 -0800

    Basic release list

--- dragondropweb/src/app/app-routing.module.ts
+++ dragondropweb/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 import {NgModule} from '@angular/core';
+import {ReleaseComponent} from './release/release.component';
+import {ReleaseListComponent} from './release-list/release-list.component';
 
 const routes: Routes = [{
   path: '',
   component: HomeComponent
+}, {
+  path: 'releases/:branch',
+  component: ReleaseComponent
+}, {
+  path: 'releases',
+  component: ReleaseListComponent
 }];
 
 @NgModule({
