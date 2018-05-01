5c93d1eba7725a979a1d8db16202352f6ca624af
Luke Powell <raistlinpowell@gmail.com>
2018-05-01 15:36:42 -0700

    Basic JWT based authentication
    - Users need to be directly added through the mongo shell at the moment.

---
+++ dragondropweb/src/app/login/login.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  email: string;
+  password: string;
+
+  constructor(private authService: AuthService) {
+  }
+
+  ngOnInit() {
+  }
+
+  login() {
+    console.log('login');
+    this.authService.login(this.email, this.password, () => {
+      // Set error message
+    });
+  }
+}
