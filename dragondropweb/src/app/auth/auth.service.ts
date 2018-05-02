e7ea97f8d1a83440f2043b13b3fac403e41d3a20
Luna Neumeister <luna.neumeister@proton.me>
2018-05-02 16:24:12 -0700

    Remove some debug loggin

--- dragondropweb/src/app/auth/auth.service.ts
+++ dragondropweb/src/app/auth/auth.service.ts
@@ -55,7 +55,6 @@ export class AuthService {
 
   public isAdmin(): boolean {
     const decodedToken = this.jwtHelperService.decodeToken();
-    console.log(decodedToken);
     return this.isAuthenticated() && (decodedToken.data.roles && decodedToken.data.roles.includes('site-admin'));
   }
 }
