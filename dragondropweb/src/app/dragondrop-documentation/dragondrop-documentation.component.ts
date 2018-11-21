8cfed919c7a52b2992f1397e640b2072120b648b
Unknown <aeon.williams@digipen.edu>
2018-11-21 11:15:42 -0800

    WIP DRAG-697
    Changes tab icon from Angular logo to DragonDrop logo. Removes Admin tab from website. Adds Documentation tab on website.

---
+++ dragondropweb/src/app/dragondrop-documentation/dragondrop-documentation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dragondrop-documentation',
+  templateUrl: './dragondrop-documentation.component.html',
+  styleUrls: ['./dragondrop-documentation.component.css']
+})
+export class DragondropDocumentationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
