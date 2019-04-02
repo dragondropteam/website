32ce2781f65e6aa5665e79461ba9c6649435d107
Luke Powell <raistlinpowell@gmail.com>
2019-04-01 23:23:43 -0700

    VueJS Project Init

---
+++ dragondrop-website/src/router.ts
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Home from './views/Home.vue';
+
+Vue.use(Router);
+
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Home,
+        },
+        {
+            path: '/download',
+            name: 'download',
+            component: Home,
+        },
+        {
+            path: '/instructions',
+            name: 'instructions',
+            component: Home,
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: Home,
+        },
+        {
+            path: '/documentation',
+            name: 'documentation',
+            component: Home,
+        },
+    ],
+});
